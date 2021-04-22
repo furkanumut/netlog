668c669aceec80fc6faf146902a3d9373c581ee3
Furkan Umut Ceylan <facfur3@gmail.com>
2021-04-22 21:16:32 +0300

    meta tag edit, composer package update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,19 +8,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', setting('site.title')) | {{ setting('site.title') }}</title>
     <meta name="description"
-        content="Jekflix is a template for Jekyll inspired by Netflix and made by Thiago Rossener.">
+        content="@yield('description', setting('site.description')) | {{ setting('site.description') }}">
     <!-- Social: Twitter -->
     <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:title" content="Jekflix | A blog theme for Jekyll">
+    <meta name="twitter:title" content="@yield('title', setting('site.title')) | {{ setting('site.title') }}">
     <meta name="twitter:description"
-        content="Jekflix is a template for Jekyll inspired by Netflix and made by Thiago Rossener.">
-    <meta property="twitter:image:src" content="{{ asset('assets/img/blog-image.png') }}">
+        content="@yield('description', setting('site.description')) | {{ setting('site.description') }}">
+    <meta property="twitter:image:src" content="@yield('image', setting('site.logo')) | {{ setting('site.logo') }}">
     <meta property="og:url" content="/">
-    <meta property="og:title" content="Jekflix | A blog theme for Jekyll">
-    <meta property="og:image" content="{{ asset('assets/img/blog-image.png') }}">
+    <meta property="og:title" content="@yield('title', setting('site.title')) | {{ setting('site.title') }}">
+    <meta property="og:image" content="@yield('image', setting('site.logo')) | {{ setting('site.logo') }}">
     <meta property="og:description"
-        content="Jekflix is a template for Jekyll inspired by Netflix and made by Thiago Rossener.">
-    <meta property="og:site_name" content="Jekflix | A blog theme for Jekyll"> <!-- Favicon -->
+        content="@yield('description', setting('site.description')) | {{ setting('site.description') }}">
+    <meta property="og:site_name" content="@yield('title', setting('site.title')) | {{ setting('site.title') }}"> <!-- Favicon -->
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon" /> <!-- Apple Touch Icons -->
     <link rel="apple-touch-icon" href="{{ asset('assets/img/icons/apple-touch-icon.png') }}" />
     <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('assets/img/icons/apple-touch-icon-57x57.png') }}" />
