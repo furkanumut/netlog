21f277c13106da3e29baad726ecca48570ff00ee
Furkan Umut Ceylan <facfur3@gmail.com>
2022-04-19 02:58:33 +0300

    Home page title edit

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('title', setting('site.title')) | {{ setting('site.title') }}</title>
+    <title>@yield('title', __('Home Page')) | {{ setting('site.title') }}</title>
     <meta name="description"
         content="@yield('description', setting('site.description')) | {{ setting('site.description') }}">
     <!-- Social: Twitter -->
