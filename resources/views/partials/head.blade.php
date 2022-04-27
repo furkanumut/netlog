7a542a4bf0752644f51dff86db916f5796200af7
Furkan Umut Ceylan <facfur3@gmail.com>
2022-04-28 01:38:04 +0300

    unnecessary meta tags removed

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,41 +1,29 @@
 <head>
     <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
     <meta charset="utf-8">
+    <title>@yield('title', __('Home Page')) | {{ setting('site.title') }}</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('title', __('Home Page')) | {{ setting('site.title') }}</title>
-    <meta name="description"
-        content="@yield('description', setting('site.description')) | {{ setting('site.description') }}">
-    <!-- Social: Twitter -->
-    <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:title" content="@yield('title', setting('site.title')) | {{ setting('site.title') }}">
-    <meta name="twitter:description"
-        content="@yield('description', setting('site.description')) | {{ setting('site.description') }}">
-    <meta property="twitter:image:src" content="@yield('image', setting('site.logo')) | {{ setting('site.logo') }}">
+    <meta name="robots" content="index, follow">
+    <meta name="description" content="@yield('description', setting('site.description'))">
+    @hasSection('keywords') <meta name=”keywords” content="@yield('keywords')"> @endif
+    @hasSection('author') <meta name="author" content="@yield('author')"> @endif
     <meta property="og:url" content="/">
     <meta property="og:title" content="@yield('title', setting('site.title')) | {{ setting('site.title') }}">
-    <meta property="og:image" content="@yield('image', setting('site.logo')) | {{ setting('site.logo') }}">
+    <meta property="og:image" content="@yield('image', setting('site.logo'))">
     <meta property="og:description"
-        content="@yield('description', setting('site.description')) | {{ setting('site.description') }}">
+        content="@yield('description', setting('site.description'))">
     <meta property="og:site_name" content="@yield('title', setting('site.title')) | {{ setting('site.title') }}"> <!-- Favicon -->
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon" /> <!-- Apple Touch Icons -->
     <link rel="apple-touch-icon" href="{{ asset('assets/img/icons/apple-touch-icon.png') }}" />
-    <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('assets/img/icons/apple-touch-icon-57x57.png') }}" />
-    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('assets/img/icons/apple-touch-icon-72x72.html') }}" />
-    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('assets/img/icons/apple-touch-icon-114x114.png') }}" />
-    <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('assets/img/icons/apple-touch-icon-144x144.png') }}" />
-    <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('assets/img/icons/apple-touch-icon-60x60.png') }}" />
-    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('assets/img/icons/apple-touch-icon-120x120.png') }}" />
-    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/icons/apple-touch-icon-76x76.html') }}" />
-    <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('assets/img/icons/apple-touch-icon-152x152.png') }}" />
     <!-- Windows 8 Tile Icons -->
-    <meta name="application-name" content="Jekflix">
+    <meta name="application-name" content="{{ setting('site.title') }}">
     <meta name="msapplication-TileColor" content="#141414">
     <meta name="theme-color" content="#141414">
-    <link href="https://fonts.googleapis.com/css?family=Titillium+Web:300,400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Titillium+Web:300,400,700&display=swap" rel="preload" as="font">
     <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}">
     <link rel="alternate" type="application/rss+xml" title="Jekflix | A blog theme for Jekyll" href="{{ asset('feed.xml') }}" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous"> -->
     <!-- JavaScript enabled/disabled -->
     <script>
         document.querySelector('html').classList.remove('no-js');
