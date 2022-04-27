0f7ee25b71872dfa38e6d766b9e560076095363f
Furkan Umut Ceylan <facfur3@gmail.com>
2022-04-28 01:35:50 +0300

    google analytics code fixed

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -10,15 +10,14 @@
             href="https://yazilimcibiradam.com/">Furkan Umut Ceylan</a> </p>
 </footer>
 <!-- END FOOTER  -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js" integrity="sha512-6PM0qYu5KExuNcKt5bURAoT6KCThUmHRewN3zUFNaoI6Di7XJPTMoT6K0nsagZKk2OB4L7E3q1uQKHNHd4stIQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     @if (setting('site.google_analytics_tracking_id') AND config('APP_ENV') != 'local' )
     <script async src="https://www.googletagmanager.com/gtag/js?id={{ setting('site.google_analytics_tracking_id') }}"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){window.dataLayer.push(arguments);}
         gtag('js', new Date());
-
         gtag('config', '{{ setting('site.google_analytics_tracking_id') }}');
+    (function(){var cssMain=document.createElement('link');cssMain.href='{{ asset("assets/css/styles.css") }}';cssMain.rel='stylesheet';cssMain.type='text/css';document.getElementsByTagName('head')[0].appendChild(cssMain);var cssFa=document.createElement('link');cssFa.href='https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css';cssFa.rel='stylesheet';cssFa.type='text/css';document.getElementsByTagName('head')[0].appendChild(cssFa)})();
     </script>
 
     @endif
